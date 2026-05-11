ed3cd8fc8d16b5c4618c9fbfe1e50981b6960c18
lql <2972400479@qq.com>
2026-05-11 23:22:39 +0800

    提交哈夫曼编码的简单介绍，优化代码结构

---
+++ main.c
@@ -0,0 +1,17 @@
+/**
+ * main.c
+ * 哈夫曼编码：文本压缩
+ * @author lql
+ * @date 2026-05-08
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
