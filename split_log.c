da1527a947252dd1b64d1b2c3fd96e022065bacd
lql <2972400479@qq.com>
2026-05-31 11:12:41 +0800

    新增日志切分模块

---
+++ split_log.c
@@ -0,0 +1,12 @@
+/**
+ * split_log.c
+ * 哈夫曼编码：历史大日志文件动态块切分与无损覆盖
+ * @author lql
+ * @date 2026-05-31
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "split_log.h"
+
---
+++ split_log.h
@@ -0,0 +1,13 @@
+/**
+ * split_log.h
+ * 哈夫曼编码：日志切分模块头文件
+ * @author lql
+ * @date 2026-05-31
+ */
+
+#ifndef SPLIT_LOG_H
+#define SPLIT_LOG_H
+
+void split_log_file_ui(void);
+
+#endif // SPLIT_LOG_H
